e7c1d5b3df05edf61371fda35dc9a2975d9bee75
Roshan Jain <roshanjain.220407@gmail.com>
2024-07-07 21:29:28 +0530

    Prealoader Completed

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var tl = gsap.timeline({
+        onComplete: () => {
+            gsap.to("#preloader", { duration: 1, autoAlpha: 0 });
+            document.getElementById("main-content").style.display = "block";
+        }
+    });
+    tl.to(".loader-content1", { duration: 0.7, autoAlpha: 1, stagger: 0.5})
+      .to("#text1", { duration: 0.3, autoAlpha: 0, delay: 0.3 })
+      .to(".loader-content2", { duration: 0.5, autoAlpha: 1, stagger: 0.5 })
+      .to("#text2", { duration: 0.5, autoAlpha: 0, delay: 0.5 })
+      .to("#text3", { duration: 0.5, autoAlpha: 1 })
+      .to("#text3", { duration: 0.5, scale: 70, delay: 0.5 });
+});
