2103ec085470e077e1309237932f079304cc25d6
Roshan Jain <roshanjain.220407@gmail.com>
2024-07-13 12:58:53 +0530

    Error

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 function showSidebar(){
     let sidebar = document.querySelector('.sidebar');
     sidebar.style.display = 'flex';
@@ -7,3 +8,19 @@ function hideSidebar(){
     let sidebar = document.querySelector('.sidebar');
     sidebar.style.display = 'none';
 }
+=======
+document.addEventListener("DOMContentLoaded", function() {
+    var tl = gsap.timeline({
+        onComplete: () => {
+            gsap.to("#preloader", { duration: 1, autoAlpha: 0 });
+            document.getElementById("main-content").style.display = "block";
+        }
+    });
+    tl.to(".loader-content1", { duration: 0.7, autoAlpha: 1, stagger: 0.5})
+      .to("#text1", { duration: 0.3, autoAlpha: 0, delay: 0.3 })
+      .to(".loader-content2", { duration: 0.5, autoAlpha: 1, stagger: 0.5 })
+      .to("#text2", { duration: 0.5, autoAlpha: 0, delay: 0.5 })
+      .to("#text3", { duration: 0.5, autoAlpha: 1 })
+      .to("#text3", { duration: 0.5, scale: 70, delay: 0.5 });
+});
+>>>>>>> main
