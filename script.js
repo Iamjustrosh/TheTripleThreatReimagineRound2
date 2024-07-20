dc55dfb9c3561fa43826c51b8c2834f03c6d905c
Rakesh250305 <152499500+Rakesh250305@users.noreply.github.com>
2024-07-20 16:09:35 +0530

    Fixed Product section and Footer

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-
-function showSidebar(){
+function showSidebar() {
     let sidebar = document.querySelector('.sidebar');
     sidebar.style.display = 'flex';
 }
 
-function hideSidebar(){
+function hideSidebar() {
     let sidebar = document.querySelector('.sidebar');
     sidebar.style.display = 'none';
 }
 
-function preLoader(){
+function preLoader() {
     document.addEventListener("DOMContentLoaded", function() {
         var tl = gsap.timeline({
             onComplete: () => {
@@ -17,46 +16,47 @@ function preLoader(){
                 document.getElementById("main-content").style.display = "block";
             }
         });
-        tl.to(".loader-content1", { duration: 0.7, autoAlpha: 1, stagger: 0.5})
-          .to("#text1", { duration: 0.3, autoAlpha: 0, delay: 0.3 })
-          .to(".loader-content2", { duration: 0.5, autoAlpha: 1, stagger: 0.5 })
-          .to("#text2", { duration: 0.5, autoAlpha: 0, delay: 0.5 })
-          .to("#text3", { duration: 0.5, autoAlpha: 1 })
-          .to("#text3", { duration: 0.5, scale: 70, delay: 0.5 });
+        tl.to(".loader-content1", { duration: 0.7, autoAlpha: 1, stagger: 0.5 })
+            .to("#text1", { duration: 0.3, autoAlpha: 0, delay: 0.3 })
+            .to(".loader-content2", { duration: 0.5, autoAlpha: 1, stagger: 0.5 })
+            .to("#text2", { duration: 0.5, autoAlpha: 0, delay: 0.5 })
+            .to("#text3", { duration: 0.5, autoAlpha: 1 })
+            .to("#text3", { duration: 0.5, scale: 70, delay: 0.5 });
     });
-    
+
 }
 
 // preLoader();
 
-function menuTab(){    
+function menuTab() {
     var menu = document.querySelector(".menu");
     var cross = document.querySelector(".cross");
 
     var tl = gsap.timeline();
-    tl.to(".sidebar",{
-        right:0,
-        duration:0.5
+    tl.to(".sidebar", {
+        right: 0,
+        duration: 0.5
     })
-    tl.from(".sidebar li",{
-        opacity:0,
-        y:50,
-        stagger:0.1,
-        duration:0.5
+    tl.from(".sidebar li", {
+        opacity: 0,
+        y: 50,
+        stagger: 0.1,
+        duration: 0.5
     })
     tl.pause();
-    menu.addEventListener("click",function(){
+    menu.addEventListener("click", function() {
         tl.play();
     })
-    cross.addEventListener("click",function(){
+    cross.addEventListener("click", function() {
         tl.reverse();
     })
 }
 menuTab();
-gsap.to(".marquee-content",{
+gsap.to(".marquee-content", {
     transform: 'translateX(-100%)',
     duration: 15,
-    repeat:-1,
-    ease:"none"
+    repeat: -1,
+    ease: "none"
 })
 
+//Product-section
